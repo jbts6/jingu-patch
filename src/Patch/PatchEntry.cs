e3118b94770d3207a3687e33cc7bbb2157f6de76
jbts6 <fh345392977@gmail.com>
2026-05-17 14:58:26 +0800

    ci(github actions): 调整触发构建的分支条件
    refactor(patch): 更新补丁逻辑与注释，优化猎弓钓竿初始化

--- src/Patch/PatchEntry.cs
+++ src/Patch/PatchEntry.cs
@@ -12,57 +12,30 @@ namespace JinguModPatch
             Loaded = true;
         }
 
-        /* …˙ªÓºº“’ 100±∂æ≠—È */
+        /* ÈááÈõÜ 100 ÂÄçÁªèÈ™å */
         public static void Prefix_SaveData_AddEffectValue(EffectId id, ref float add, bool needTips)
         {
             add *= 100f;
         }
 
-        /* –ﬁ∏ƒ¡‘π≠ ˝æ› */
-        public static bool Prefix_HuntBow_Init()
+        /* Áã©ÁåéÂàùÂßãÁåéÂºì */
+        public static void Postfix_HuntBow_Init()
         {
-            HuntBow.m_dic = new Dictionary<int, HuntBowData>(8);
-            HuntBowData huntBowData = new HuntBowData(230001, 220, 300, 500);
-            HuntBow.m_dic[huntBowData.m_id] = huntBowData;
-            huntBowData = new HuntBowData(230002, 220, 300, 500);
-            HuntBow.m_dic[huntBowData.m_id] = huntBowData;
-            huntBowData = new HuntBowData(230003, 220, 300, 500);
-            HuntBow.m_dic[huntBowData.m_id] = huntBowData;
-            huntBowData = new HuntBowData(230004, 220, 300, 500);
-            HuntBow.m_dic[huntBowData.m_id] = huntBowData;
-            huntBowData = new HuntBowData(230005, 220, 300, 500);
-            HuntBow.m_dic[huntBowData.m_id] = huntBowData;
-            huntBowData = new HuntBowData(230006, 220, 300, 500);
-            HuntBow.m_dic[huntBowData.m_id] = huntBowData;
-            huntBowData = new HuntBowData(230007, 220, 300, 500);
-            HuntBow.m_dic[huntBowData.m_id] = huntBowData;
-            huntBowData = new HuntBowData(230008, 220, 300, 500);
-            HuntBow.m_dic[huntBowData.m_id] = huntBowData;
-            return false;
+            if (HuntBow.m_dic != null && HuntBow.m_dic.ContainsKey(230001))
+            {
+                HuntBow.m_dic[230001] = new HuntBowData(230001, 220, 300, 500);
+            }
         }
 
 
-        /* –ﬁ∏ƒ”Êæﬂ ˝æ› */
-        public static bool Prefix_FishRod_Init()
+        /* Áã©ÁåéÂàùÂßãÈíìÁ´ø */
+        public static void Postfix_FishRod_Init()
         {
-            FishRod.m_dic = new Dictionary<int, FishRodData>(8);
-            FishRodData fishRodData = new FishRodData(220001, 300, 300, 70);
-            FishRod.m_dic[fishRodData.m_id] = fishRodData;
-            fishRodData = new FishRodData(220002, 300, 300, 70);
-            FishRod.m_dic[fishRodData.m_id] = fishRodData;
-            fishRodData = new FishRodData(220003, 300, 300, 70);
-            FishRod.m_dic[fishRodData.m_id] = fishRodData;
-            fishRodData = new FishRodData(220004, 300, 300, 70);
-            FishRod.m_dic[fishRodData.m_id] = fishRodData;
-            fishRodData = new FishRodData(220005, 300, 300, 70);
-            FishRod.m_dic[fishRodData.m_id] = fishRodData;
-            fishRodData = new FishRodData(220006, 300, 300, 70);
-            FishRod.m_dic[fishRodData.m_id] = fishRodData;
-            fishRodData = new FishRodData(220007, 300, 300, 70);
-            FishRod.m_dic[fishRodData.m_id] = fishRodData;
-            fishRodData = new FishRodData(220008, 300, 300, 70);
-            FishRod.m_dic[fishRodData.m_id] = fishRodData;
-            return false;
+            if (FishRod.m_dic != null && FishRod.m_dic.ContainsKey(220001))
+            {
+                FishRod.m_dic[220001] = new FishRodData(220001, 300, 300, 80);
+            }
+            
         }
     }
 }
